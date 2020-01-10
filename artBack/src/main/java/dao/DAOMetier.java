bfad6c6b6b2e8efc90507085dd9d8cb2fd15028b
Alban <alban.delouvee@gmail.com>
2020-01-10 11:24:28 +0100

    alban

---
+++ artBack/src/main/java/dao/DAOMetier.java
@@ -0,0 +1,10 @@
+package dao;
+
+import java.util.List;
+
+import model.Metier;
+
+public interface DAOMetier extends DAO<Metier,Integer>
+{
+	public List<Metier> selectById(String titre_metier);
+}
